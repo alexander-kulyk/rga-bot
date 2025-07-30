62e8c9287ce99716303f5fef2d64a214da1e3019
Oleksandr Kulyk <oleku@smart-it.com>
2025-07-30 09:26:16 +0300

    Refactor loadDocx to use TokenTextSplitter for improved token management

--- Server/helpers/loadDocx.js
+++ Server/helpers/loadDocx.js
@@ -1,5 +1,8 @@
 //core
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import {
+  RecursiveCharacterTextSplitter,
+  TokenTextSplitter,
+} from 'langchain/text_splitter';
 import mammoth from 'mammoth';
 //other
 import getChunkModel from '../models/manualChunkSchema.js';
@@ -15,17 +18,26 @@ const loadDocx = async (buffer, fileName) => {
 
   const ChunkModel = getChunkModel(collName);
 
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000, // target ~1000 characters per chunk
-    chunkOverlap: 200, // 200 characters overlap between neighbors
-    separators: ['\n\n', '\n', ' ', ''],
+  // const splitter = new RecursiveCharacterTextSplitter({
+  //   chunkSize: 1000, // target ~1000 characters per chunk
+  //   chunkOverlap: 200, // 200 characters overlap between neighbors
+  //   separators: ['\n\n', '\n', ' ', ''],
+  // });
+
+  const splitter = new TokenTextSplitter({
+    encodingName: 'cl100k_base', // Encoding for OpenAI's models
+    chunkSize: 500, // target ~500 tokens per chunk
+    chunkOverlap: 50,
   });
 
   try {
     const result = await mammoth.extractRawText({ buffer });
-    const docs = await splitter.createDocuments([result.value]);
+
+    //const splittedDocument = await splitter.createDocuments([result.value]);
+    const splittedDocument = await splitter.createDocuments([result.value]);
+
     await ChunkModel.insertMany(
-      docs.map((doc, i) => ({
+      splittedDocument.map((doc, i) => ({
         pageContent: doc.pageContent,
         metadata: doc.metadata,
       }))
